ad730a052c0c9d6434cdb3d515731cb6d104913d
Robyn <github@robyn.zip>
2024-12-08 23:41:24 +0000

    Day 4 part 1 complete

--- 04/01.php
+++ 04/01.php
@@ -1,57 +1,95 @@
 <?php
 
 function getPuzzleInput() {
-	$puzzleInput = file_get_contents('input.txt');
+	$puzzleInput = file_get_contents('input-01.txt');
 	$puzzleRows = explode("\n", $puzzleInput);
-
-	return $puzzleRows;
+	return array_map('str_split', $puzzleRows);
 }
 
-function charMatrixRotate($inputRows) {
-	$characterRows = array();
-	$newMatrix = array();
 
-	foreach ($inputRows as $inputRow) {
-		$newMatrix[] = array();
-	}
-
-	for ($i = 0; $i < count($inputRows); $i++) {
-		$chars = str_split($inputRows[$i]);
-		for ($j = 0; $j < count($chars); $j++) {
-			$newMatrix[$j][$i] = $chars[$j];
+function findAllStartingChars(&$puzzleIn): array {
+	$xLocations = array();
+	for ($y = 0; $y < sizeof($puzzleIn); $y++) {
+		for ($x = 0; $x < sizeof($puzzleIn[$y]); $x++) {
+			if ($puzzleIn[$y][$x] === "X") {
+				$xLocations[] = [$y, $x];
+			}
 		}
 	}
+	return $xLocations;
+}
 
-	foreach ($newMatrix as $newMatrixRow) {
-		$characterRows[] = implode($newMatrixRow);
+function findAll(&$puzzleIn, &$searchLocations): int {
+	$found = 0;
+	foreach ($searchLocations as $sl) {
+		print("Investigating X at: " . json_encode($sl) . "\n");
+
+		$topRight = "";
+		$topLeft = "";
+		$bottomRight = "";
+		$bottomLeft = "";
+		$left = "";
+		$right = "";
+		$top = "";
+		$bottom = "";
+		for ($i = 0; $i < 4; $i++) {
+			// Top right
+			if (!empty($puzzleIn[$sl[0] + $i]) && !empty($puzzleIn[$sl[0] + $i][$sl[1] + $i])) {
+				$topRight .= $puzzleIn[$sl[0] + $i][$sl[1] + $i];
+			}
+			// Top left
+			if (!empty($puzzleIn[$sl[0] + $i]) && !empty($puzzleIn[$sl[0] + $i][$sl[1] - $i])) {
+				$topLeft .= $puzzleIn[$sl[0] + $i][$sl[1] - $i];
+			}
+			// Bottom right
+			if (!empty($puzzleIn[$sl[0] - $i]) && !empty($puzzleIn[$sl[0] - $i][$sl[1] + $i])) {
+				$bottomRight .= $puzzleIn[$sl[0] - $i][$sl[1] + $i];
+			}
+			// Bottom left
+			if (!empty($puzzleIn[$sl[0] - $i]) && !empty($puzzleIn[$sl[0] - $i][$sl[1] - $i])) {
+				$bottomLeft .= $puzzleIn[$sl[0] - $i][$sl[1] - $i];
+			}
+
+			// Right
+			if (!empty($puzzleIn[$sl[0]][$sl[1] + $i])) {
+				$right .= $puzzleIn[$sl[0]][$sl[1] + $i];
+			}
+			// Left
+			if (!empty($puzzleIn[$sl[0]][$sl[1] - $i])) {
+				$left .= $puzzleIn[$sl[0]][$sl[1] - $i];
+			}
+			// Bottom
+			if (!empty($puzzleIn[$sl[0] - $i]) && !empty($puzzleIn[$sl[0] - $i][$sl[1]])) {
+				$bottom .= $puzzleIn[$sl[0] - $i][$sl[1]];
+			}
+			// Top
+			if (!empty($puzzleIn[$sl[0] + $i]) && !empty($puzzleIn[$sl[0] + $i][$sl[1]])) {
+				$top .= $puzzleIn[$sl[0] + $i][$sl[1]];
+			}
+		}
+		print ("TR: $topRight, TL: $topLeft, BR: $bottomRight, BL: $bottomLeft\n");
+		print ("T: $top, R: $right, B: $bottom, L: $left\n");
+
+		if ($topRight === "XMAS") $found++;
+		if ($topLeft === "XMAS") $found++;
+		if ($bottomRight === "XMAS") $found++;
+		if ($bottomLeft === "XMAS") $found++;
+		if ($top === "XMAS") $found++;
+		if ($right === "XMAS") $found++;
+		if ($bottom === "XMAS") $found++;
+		if ($left === "XMAS") $found++;
 	}
 
-	return array_values($characterRows);
+	return $found;
 }
-
-function countInstancesOfString($str): int {
-	return substr_count($str, 'XMAS') + substr_count($str, 'SAMX');
-}
-
 
 function part1 () {
 	$puzzleIn = getPuzzleInput();
-	$puzzleRotated = charMatrixRotate($puzzleIn);
 
-	$found = 0;
+	$searchLocations = findAllStartingChars($puzzleIn);
 
-	// Check for forwards and backwards strings in rows
-	foreach ($puzzleIn as $puzzleRow) {
-		$found += countInstancesOfString($puzzleRow);
-	}
-
-	// Check for forwards and backwards strings in columns
-	foreach ($puzzleRotated as $puzzleRow) {
-		$found += countInstancesOfString($puzzleRow);
-	}
-
-
-	print($found);
+	print_r(json_encode($searchLocations) . "\n");
+	print_r("Found: " . findAll($puzzleIn, $searchLocations));
 }
 
 part1();
